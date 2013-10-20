0bba3a2f87784dd6fd3b8d0e4687abb0d1131d46
Joseph Wilk <joe@josephwilk.net>
2013-10-20 12:50:47 +0200

    experiment with how macrolet might look in elixir

--- test/t_macro_tools.exs
+++ test/t_macro_tools.exs
@@ -1,7 +1,10 @@
 defmodule MacroToolsTest do
   use Amrita.Sweet
 
-  fact "the truth" do
-    true |> truthy
+  fact "macrofn" do
+    mac_fn = (macrofn form -> quote, do: [unquote(form), unquote(form)])
+
+    mac_fn(10) == [10, 10]
   end
+
 end
